390f3a86dda3563dee050f6f4dea38d4dfdadb9f
Kuba Karasek <kubakarasek@Kubas-MBP.home>
2024-02-26 23:46:44 +0100

    initial commit ✨

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution');
+
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-recommended',
+    '@vue/eslint-config-airbnb-with-typescript',
+    'plugin:prettier/recommended',
+    'plugin:vuejs-accessibility/recommended'
+  ],
+  plugins: ['vuejs-accessibility'],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  }
+};
